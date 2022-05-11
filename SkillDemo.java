6512f285c7f7a364c823d960e1de5146e778aaf8
MarkH857 <97138169+MarkH857@users.noreply.github.com>
2022-05-11 11:15:36 -0700

    Created a junit test

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,3 +1,6 @@
 public class SkillDemo {
   // This is our new java file!
+  public static int multiply(int a, int b) {
+    return a*b;
+  }
 }
---
+++ SkillDemoTest.java
@@ -0,0 +1,11 @@
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+    @Test
+    public void testMultiply() {
+        assertEquals(2, SkillDemo.multiply(2, 2));
+    }
+}
+// This is a test that will fail initially
